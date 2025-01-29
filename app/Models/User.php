7d2f6f6a9e7b8f37f741e960decf01c4d6f5dc8b
Juaniesd <juanromeroviudez@gmail.com>
2025-01-29 20:50:43 +0100

    migration, factories and models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -64,4 +65,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function orders() : HasMany {
+        return $this->hasMany(order::class);
+    }
+
 }
